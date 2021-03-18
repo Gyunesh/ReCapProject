81ad87741cfe7fade3b17f2307e57b48bd9993e8
sun <sun@DESKTOP-GP6QFAV>
2021-03-18 14:26:11 +0000

    18/03

--- Business/Concrete/BrandManager.cs
+++ Business/Concrete/BrandManager.cs
@@ -21,7 +21,7 @@ namespace Business.Concrete
 
         public IResult Add(Brand brand)
         {
-            if (brand.BrandName.Length >= 2)
+            if (brand.BrandName.Length <= 2)
             {
                 return new ErrorResult(Messages.BrandNameInvalid);
 
